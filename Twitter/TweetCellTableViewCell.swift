8b4d02e6f32243bda2f75537afa4860cfdd18fcb
Fianko Buckle <fiankobuckle@gmail.com>
2019-11-01 18:47:13 -0400

    tweet design

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Fianko Buckle on 11/1/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileName: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
